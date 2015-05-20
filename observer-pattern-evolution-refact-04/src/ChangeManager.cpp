43724e08baecdeff32a10cd97740716065256e92
Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
2015-05-20 19:28:50 -0300

    refact-04

---
+++ observer-pattern-evolution-refact-04/src/ChangeManager.cpp
@@ -0,0 +1,57 @@
+/**
+    File        : ChangeManager.cpp
+
+    Project     : observer-pattern-evolution-refact-02
+
+    Copyright 2015 Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
+
+    Licensed under The MIT License
+    For full copyright and license information, please see the LICENSE.txt
+    Redistributions of files must retain the above copyright notice.
+
+    @copyright     Copyright (c) Gabriel Nicolás González Ferreira
+    @license       http://www.opensource.org/licenses/mit-license.php
+**/
+
+#include "../include/ChangeManager.h"
+
+ChangeManager* ChangeManager::m_instance = 0;
+
+ChangeManager::ChangeManager()
+{
+    //ctor
+}
+
+ChangeManager::~ChangeManager()
+{
+    //dtor
+}
+
+ChangeManager* ChangeManager::getInstance()
+{
+    if(m_instance == 0)
+    {
+        m_instance = new ChangeManager();
+    }
+    return m_instance;
+}
+
+void ChangeManager::attach(IObserver* observer)
+{
+    m_observersList.push_front(observer);
+}
+
+void ChangeManager::detach(IObserver* observer)
+{
+    m_observersList.remove(observer);
+}
+
+void ChangeManager::notify( void )
+{
+    list<IObserver*>::iterator it;
+
+    for (it =  m_observersList.begin() ; it !=  m_observersList.end(); ++it)
+    {
+       (*it)->update();
+    }
+}
---
+++ observer-pattern-evolution-refact-04/src/Timer.cpp
@@ -0,0 +1,93 @@
+/**
+    File        : Timer.cpp
+
+    Project     : observer-pattern-evolution-refact-02
+
+    Copyright 2015 Gabriel Nicolás González Ferreira <gabrielinuz@gmail.com>
+
+    Licensed under The MIT License
+    For full copyright and license information, please see the LICENSE.txt
+    Redistributions of files must retain the above copyright notice.
+
+    @copyright     Copyright (c) Gabriel Nicolás González Ferreira
+    @license       http://www.opensource.org/licenses/mit-license.php
+**/
+
+#include "../include/Timer.h"
+
+Timer::Timer(INotifier* notifier)
+{
+    m_hours = 0;
+    m_minutes = 0;
+    m_seconds = 0;
+    m_notifier = notifier;
+}
+
+Timer::Timer(int hours, int minutes, int seconds, INotifier* notifier)
+{
+    m_hours = hours;
+    m_minutes = minutes;
+    m_seconds = seconds;
+    m_notifier = notifier;
+}
+
+Timer::~Timer(){m_tick();}
+
+void Timer::setHours(int hours)
+{
+    m_hours = hours;
+}
+
+int Timer::getHours()
+{
+    return m_hours;
+}
+
+void Timer::setMinutes(int minutes)
+{
+    m_minutes = minutes;
+}
+
+int Timer::getMinutes()
+{
+    return m_minutes;
+}
+
+void Timer::setSeconds(int seconds)
+{
+    m_seconds = seconds;
+}
+
+int Timer::getSeconds()
+{
+    return m_seconds;
+}
+
+void Timer::m_tick()
+{
+    while(1)
+    {
+        m_seconds++;
+        if(m_seconds == 60)
+        {
+            m_seconds = 0;
+            m_minutes++;
+            if(m_minutes == 60)
+            {
+                m_seconds = 0;
+                m_minutes = 0;
+                m_hours++;
+                if(m_hours == 24)
+                {
+                    m_seconds = 0;
+                    m_minutes = 0;
+                    m_hours = 0;
+                }
+            }
+        }
+
+        m_notifier->notify();
+        this_thread::sleep_for (chrono::seconds(1));
+    }
+}
+
